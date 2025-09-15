8799bb03ae00fa9253d5e33ff625279559f9f2d2
小防风 <1041738178@qq.com>
2025-09-15 18:01:22 +0800

    Initial Commit

--- ConsoleGame/obj/Debug/net8.0/ConsoleGame.AssemblyInfo.cs
+++ ConsoleGame/obj/Debug/net8.0/ConsoleGame.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleGame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98c911799c892c12bc8d94db98b8fa6dcb2545fd")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleGame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleGame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
